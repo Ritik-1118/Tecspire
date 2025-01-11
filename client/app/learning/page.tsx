b8f4fb1eae26ecbd9c3bf2dfa9a884a5b457caa4
DiwanshGupta <guptadiwansh@gmail.com>
2025-01-11 20:58:31 +0530

    add learning page

--- client/app/learning/page.tsx
+++ client/app/learning/page.tsx
@@ -1,12 +1,81 @@
-import * as React from 'react';
+import React from 'react';
 
-export interface IAppProps {
-}
 
-export default function App (props: IAppProps) {
+
+const learning = () => {
     return (
-        <div className="min-h-screen bg-gradient-to-t from-[#0A0A0A] via-[#050603] to-[#09120E] text-white  flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">Learning page</h1>
-        </div>
+        <section className="bg-gradient-to-t from-[#09120E] via-[#050603] to-[#0A0A0A] text-white py-16">
+            <div className="container max-w-[85rem] mx-auto text-center px-6">
+                <h2 className="text-3xl font-bold mb-6">Explore Our Internship Domains</h2>
+                <p className="text-gray-300 mb-10">
+                    Choose from a wide range of domains to kickstart your career in your desired field.
+                </p>
+                <div className="  flex flex-col">
+
+                    <div className='flex flex-col   m-3  gap-3  md:flex-row'>
+                    <div className="learn_bg text-left   hover:bg-gray-500/35  border  border-white  w-full md:w-3/4  shadow-md rounded-2xl p-6 hover:shadow-lg  transition duration-300">
+                        <h1 className="text-4xl mb-4">BCA</h1>
+                        <h3 className="text-xl font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+                    <div className='flex flex-col gap-3 m-auto  items-center'>
+                    <div className="learn_bg text-left items-center border  border-white hover:bg-gray-500/35 w-full   shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300">
+                        <span className="text-2xl mb-4">BCA</span>
+                        <h3 className="text-lg font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+
+                    <div className="learn_bg text-left w-full items-end border  border-white hover:bg-gray-500/35   shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300">
+                        <span className="text-2xl mb-4">BCA</span>
+                        <h3 className="text-lg font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+                    </div>
+                    </div>
+                   
+                    <div className='flex flex-col    mx-3  gap-3  md:flex-row'>
+                    <div className='flex flex-col gap-4 m-auto  items-center'>
+                    <div className="learn_bg text-left items-center border  border-white hover:bg-gray-500/35 w-full   shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300">
+                        <span className="text-2xl mb-4">BCA</span>
+                        <h3 className="text-lg font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+
+                    <div className="learn_bg text-left w-full items-end border  border-white hover:bg-gray-500/35   shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300">
+                        <span className="text-2xl mb-4">BCA</span>
+                        <h3 className="text-lg font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+                    </div>
+                    <div className="learn_bg text-left    border  border-white hover:bg-gray-500/35 w-full md:w-3/4  shadow-md rounded-2xl p-6 hover:shadow-lg transition duration-300">
+                        <h1 className="text-4xl mb-4">BCA</h1>
+                        <h3 className="text-xl font-semibold mb-2">Bachelor's of Computer Application
+                        </h3>
+                        <p className="text-gray-300">
+                        Access technical codes, user-friendly code snippets, simplified learning resources, and enhanced conceptual clarity.  
+                        </p>
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
     );
-}
+};
+
+export default learning;
